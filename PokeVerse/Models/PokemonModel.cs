3b0611b74855e255f338ceb5be9f185540afec45
Leanne Sall <lsall1@my.bcit.ca>
2020-12-08 11:27:49 -0800

    Added Pokemon model File

---
+++ PokeVerse/Models/PokemonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeVerse.Models
+{
+    public class PokemonModel : BaseEntity
+    {
+        public PokemonModel()
+        {
+
+
+
+        }
+    }
+}
