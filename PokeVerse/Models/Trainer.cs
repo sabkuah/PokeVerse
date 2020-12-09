c6b4660c44a2988cd0eeff03d43fb5ebce19fe11
scurrie90 <scurrie30@my.bcit.ca>
2020-12-08 19:48:52 -0800

    Created user models. #11

---
+++ PokeVerse/Models/Trainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeVerse.Models
+{
+    public class Trainer : BaseEntity
+    {
+        public int TrainerId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
