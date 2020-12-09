ef836ee8a9b6a6309984c9ea7baba7d785c01207
scurrie90 <scurrie30@my.bcit.ca>
2020-12-09 03:28:41 -0800

    Added variables I didn't save in my initial push.

--- PokeVerse/Models/Trainer.cs
+++ PokeVerse/Models/Trainer.cs
@@ -8,8 +8,17 @@ namespace PokeVerse.Models
     public class Trainer : BaseEntity
     {
         public int TrainerId { get; set; }
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string Email { get; set; }
         public DateTime DateJoined { get; set; }
+
+        public Trainer(int trainerId, string username, string email, DateTime dateJoined)
+        {
+            trainerId = 0;
+            username = trainer;
+            email = emailaddress;
+            dateJoined = date;
+
+        }
     }
 }
