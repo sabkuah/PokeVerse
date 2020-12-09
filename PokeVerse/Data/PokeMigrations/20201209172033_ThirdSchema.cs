60cf402d68215d2f3cdc651b6eda15cbf9923e4a
Leanne Sall <lsall1@my.bcit.ca>
2020-12-09 09:22:49 -0800

    Closes #43 with caveat of having to delete database first and run different schema

---
+++ PokeVerse/Data/PokeMigrations/20201209172033_ThirdSchema.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PokeVerse.Data.PokeMigrations
+{
+    public partial class ThirdSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
