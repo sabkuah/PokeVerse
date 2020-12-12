bebbb27897628088ad4d5f70cba5762aaca07ad0
Shenxuan Wang <swang282@my.bcit.ca>
2020-12-11 17:35:31 -0800

    pagination works in All filter

--- PokeVerse/Pages/Catalogue.cshtml.cs
+++ PokeVerse/Pages/Catalogue.cshtml.cs
@@ -8,24 +8,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using PokeVerse.Interfaces;
 using PokeVerse.Models;
+using PokeVerse.Services;
 using PokeVerse.ViewModels;
 
 namespace PokeVerse.Pages
 {
     public class CatalogueModel : PageModel
     {
-        private readonly IPokemonVMService _pokemonVMService;
-
-        public CatalogueModel(IPokemonVMService pokemonVMService)
+        private readonly IPokemonVMService _IpokemonVMService;
+        public CatalogueModel(IPokemonVMService ipokemonVMService)
         {
-            _pokemonVMService = pokemonVMService;
+            _IpokemonVMService = ipokemonVMService;
         }
 
         [BindProperty(SupportsGet = true)]
         public int CurrentPage { get; set; } = 1;
 
         public int Count { get; set; } = 150;
-        public int PageSize { get; set; } = 12;
+        public int PageSize { get; set; } = 6;
 
         public int TotalPages => (int)Math.Ceiling(decimal.Divide(Count, PageSize));
 
@@ -34,10 +34,11 @@ namespace PokeVerse.Pages
 
 
         public PokemonIndexVM PokemonIndex = new PokemonIndexVM();
-       
+
         public async Task OnGet(PokemonIndexVM pokemonIndex)
         {
-            PokemonIndex = _pokemonVMService.GetPokemonsVM(pokemonIndex.TypesFilterApplied, CurrentPage, PageSize);
+            PokemonIndex = _IpokemonVMService.GetPokemonsVM(pokemonIndex.TypesFilterApplied, CurrentPage, PageSize);
+
         }
     }
 }
