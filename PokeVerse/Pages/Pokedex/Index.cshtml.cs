c00c70cbc91a7cf902ed3e2e78ae0c5987cd839b
Leanne Sall <lsall1@my.bcit.ca>
2020-12-14 11:11:15 -0800

    Revert "Merge branch 'main' of https://github.com/BCIT-SSD-2020-21/dotnetproject-team2_netproject into main"
    This reverts commit 0d2f5ba2e20e34f84a34123abd8a3a7d802c46ad, reversing
    changes made to b3e62e5b37836d92c8c021b69c355bc86e274a21.

--- PokeVerse/Pages/Pokedex/Index.cshtml.cs
+++ PokeVerse/Pages/Pokedex/Index.cshtml.cs
@@ -105,7 +105,6 @@ namespace PokeVerse.Pages.Pokedex
                 PokedexPokemon p = new PokedexPokemon(TrainerPokedex.Id, testPokemon.Id);
                 p.Pokemon = new Pokemon(testPokemon.PokeNumber, testPokemon.Name, testPokemon.Type0, testPokemon.Type1, testPokemon.Attack, testPokemon.Defense,  testPokemon.Speed);
                 TrainerPokedex.PokedexPokemons.Add(p);
-                _db.Remove(p.Pokemon);
                 _db.SaveChanges();
 
             }
