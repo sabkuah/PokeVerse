7e6925a32d101c2e3b810c155a2a072433552ce1
Leanne Sall <lsall1@my.bcit.ca>
2020-12-15 11:10:17 -0800

    Fixed doubled error x3

--- PokeVerse/Pages/Pokedex/Index.cshtml.cs
+++ PokeVerse/Pages/Pokedex/Index.cshtml.cs
@@ -84,10 +84,9 @@ namespace PokeVerse.Pages.Pokedex
             try
             {
                 PokedexPokemon p = new PokedexPokemon(TrainerPokedex.Id, testPokemon.Id);
-                p.Pokemon = new Pokemon(testPokemon.PokeNumber, testPokemon.Name, testPokemon.Type0, testPokemon.Type1, testPokemon.Attack, testPokemon.Defense,  testPokemon.Speed);
-                _db.Pokemon.Remove(p.Pokemon);
                 TrainerPokedex.PokedexPokemons.Add(p);
                 _db.SaveChanges();
+                p.Pokemon = new Pokemon(testPokemon.PokeNumber, testPokemon.Name, testPokemon.Type0, testPokemon.Type1, testPokemon.Attack, testPokemon.Defense, testPokemon.Speed);
 
             }
             catch
